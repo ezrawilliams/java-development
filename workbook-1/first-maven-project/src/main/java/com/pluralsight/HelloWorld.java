ceeafbd68f957c93deb5b701f552a0e1c8fade55
Ezra A Williams <ewilliamsdaniel@gmail.com>
2024-04-09 11:06:23 -0400

    completed ShoppingList app

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class HelloWorld {
+    //This is the MAIN method - entry point in the JVM
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Item 1");
+        System.out.println("Item 2");
+        System.out.println("Item 3");
+        System.out.println("Item 4");
+        System.out.println("Item 5");
+        System.out.println("Item 6");
+        System.out.println("Item 7");
+        System.out.println("Item 8");
+        System.out.println("Item 9");
+        System.out.println("Item 10");
+    }
+}
